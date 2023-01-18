f7b79a2210088110296a85fdeed127ea72318d6a
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2023-01-18 16:26:44 +0100

    created react app and folder structures for the car app website

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../../styles/header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__top">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
+              <div className="header__top__left">
+                <span>Need Help?</span>
+                <span className="header__top__help">
+                  <i class="ri-phone-fill"></i> +1-2046576688
+                </span>
+              </div>
+            </Col>
+            <Col lg="6" md="6" sm="6">
+              <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
+                <Link to="#" className=" d-flex align-items-center gap-1">
+                  <i class="ri-login-circle-line"></i>Login
+                </Link>
+                <Link to="#" className=" d-flex align-items-center gap-1">
+                  <i class="ri-user-line"></i>Register
+                </Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      {/* header middle */}
+      <div className="header__middle">
+        <Container>
+          <Row>
+            <Col lg="4" md="3" sm="4">
+              <div className="logo">
+                <h1>
+                  <Link to="/home" className="d-flex align-items-center gap-3">
+                    <i class="ri-car-line"></i>
+                    <span>
+                      Rent Car <br />
+                      Service
+                    </span>
+                  </Link>
+                </h1>
+              </div>
+            </Col>
+            <Col lg="3" md="3" sm="4">
+              <div className="header__location d-flex align-items-center gap-2">
+                <span>
+                  <i class="ri-earth-line"></i>
+                </span>
+                <div className="header__location-content">
+                  <h4>Berlin</h4>
+                  <h6>Bochum, Mainz, Dortmund</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="3" md="3" sm="4">
+              <div className="header__location d-flex align-items-center gap-2">
+                <span>
+                  <i class="ri-time-line"></i>
+                </span>
+                <div className="header__location-content">
+                  <h4>Monday to Friday</h4>
+                  <h6>10am - 7pm</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="2" md="3" sm="0" className="text-end">
+              <button className="header__btn btn">
+                <Link to="/contact">
+                  <i class="ri-phone-line"></i>Request a call
+                </Link>
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      {/* main navigation */}
+      <div className="main__navbar">
+        <Container>
+          <div className="navigation__wrapper d-flex align-items-center justify-content-between">
+            <span className="mobile__menu">
+              <i class="ri-menu-line"></i>
+            </span>
+            <div className="navigation">
+              <div className="menu"></div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
